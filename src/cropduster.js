c11fa50aaea546f84de374f38c5f24b1e45ee7e9
istateside <kevinrfleischman@gmail.com>
2018-01-08 11:10:36 -0500

    fixes compilation error

--- src/cropduster.js
+++ src/cropduster.js
@@ -344,7 +344,7 @@ const CD = {
     let hash = 0;
     if (str.length === 0) return hash;
 
-    for (const i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
       hash = ((hash << 5) - hash + str.charCodeAt(i)) & 0xffffffff;
     }
 
